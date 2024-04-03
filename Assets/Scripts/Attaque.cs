886630d77f6fe089e506feb9cf08b5271eb342b2
Dakeyrass <pwkmarion@gmail.com>
2024-04-03 15:56:05 +0200

    Récupération
    Théoriquement arme + changement de scène + essai pour Iframe

--- Assets/Scripts/Attaque.cs
+++ Assets/Scripts/Attaque.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class Attaque : MonoBehaviour
 {
     [SerializeField] private BoxCollider2D atk;
+    private bool hasWeapon = false;
     // Start is called before the first frame update
     void Start()
     {
         atk.enabled = false;
+        hasWeapon = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && hasWeapon);
         {
             Attack();
+        }
+    }
+
+    //Si le joueur rencontre l'arme, il la récupère, elle disparait + il peut attaquer (waw)
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       if (other.CompareTag("Arme"))
+        {
+            hasWeapon = true;
+            //et pouf plus d'arme sur la map
+            other.gameObject.SetActive(false);
         }
     }
 
--- Assets/Scripts/Controles.cs
+++ Assets/Scripts/Controles.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Controles : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class Controles : MonoBehaviour
     [SerializeField] private Text collectible_counter;
     private int collectible = 0;
     private BoxCollider2D col;
+    private bool invincible = false;
 
 
     // Start is called before the first frame update
@@ -32,20 +34,52 @@ public class Controles : MonoBehaviour
         //On applique la v�locit� au rgdbd et on la multiplie par la vitesse souhait�e
         deplacements.Normalize();
         body.velocity = deplacements * speed;
+
+        //FLIP
+        if (horizontal > 0)
+        {
+            transform.localScale = new Vector3(1f, 1f);
+        }
+        else if (horizontal < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f);
+        }
     }
 
     private void OnTriggerEnter2D (Collider2D other){
-        if (other.CompareTag("Ennemi")){
-            pv -= 1;
-            if (pv<=0){
-                Destroy(gameObject);
-            }
-        }
         if (other.CompareTag("Collectible"))
         {
             collectible += 1;
             collectible_counter.text = "" + collectible;
             Destroy(other.gameObject);
         }
+        //mort du joueur et perte de pv  
+        if (other.CompareTag("Ennemi") && !invincible)
+        {
+            pv -= 1;
+            Debug.Log(pv);
+            if (pv == 0)
+            {
+                restart();
+            }
+            else
+            {
+                invincible = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+    public void IFrames()
+    {
+        Debug.Log("fin");
+        invincible = false;
+    }
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
